b5bcfa36afd38e161fcbbf61cf47255d2a5ce681
davemann100 <davemann100@gmail.com>
2023-03-29 16:27:51 -0700

    wip js functionality

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('clog is working');
+//import { key } from "./apiKey.mjs";
+const key = 0; 
+
+
+var currentCity = "";
+var cityInput = document.querySelector('#citySearch');
+var cityName = document.querySelector('#cityName');
+
+function getCity(event){
+    console.log(event.target.value);
+    currentCity = event.target.value;
+}
+cityInput.addEventListener('input', getCity)
+
+
+function search(){
+    cityName.innerHTML = currentCity; 
+}
+
+
+//Fetch Api Data
+async function apiCall(){
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${currentCity},CA,US&cnt=40&appid=${key}&units=imperial`);
+    var apiData = await response.json();
+    console.log(apiData);
+}
+apiCall();
+
+
+
+//Loop through retrieved data 
+//  for(var i=0; i<apiData.list.length; i+=8){
+//      console.log(apiData.list[i].dt_txt + " ---> " + apiData.list[i].main.temp  + " ---> " /*+ JSON.stringify(apiData.list[i].weather)*/);
