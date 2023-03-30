8ff7b6f0960ced60ad8c0948b18ea4b71bd7365e
davemann100 <davemann100@gmail.com>
2023-03-29 20:22:06 -0700

    js functionality

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 //import { key } from "./apiKey.mjs";
-const key = 0; 
+const key = ""; 
 
 
-var currentCity = "";
-var cityInput = document.querySelector('#citySearch');
+var searchedCity = "Dublin";
 var cityName = document.querySelector('#cityName');
+var main = document.querySelector('#main');
+var temp = document.querySelector('#temperature');
+var feel = document.querySelector('#feel');
+var wind = document.querySelector('#wind');
 
-function getCity(event){
-    console.log(event.target.value);
-    currentCity = event.target.value;
+function getCity(element){
+    console.log(element.value);
+    searchedCity = element.value;
 }
-cityInput.addEventListener('input', getCity)
-
 
 function search(){
-    cityName.innerHTML = currentCity; 
+    //cityName.innerHTML = searchedCity;
+    console.log(searchedCity);
+    apiCall(); 
 }
 
 
 //Fetch Api Data
 async function apiCall(){
-    var response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${currentCity},CA,US&cnt=40&appid=${key}&units=imperial`);
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchedCity},CA,US&cnt=1&appid=${key}&units=imperial`);
     var apiData = await response.json();
-    console.log(apiData);
+    console.log(apiData); //all data
+    //console.log(apiData.list[0].weather[0].main); //main-weather retrieval
+    //console.log(apiData.list[0].main.temp);       //temperature retrieval
+    //console.log(apiData.list[0].main.feels_like); //real-feel retrieval
+    //console.log(apiData.list[0].wind.speed);      //wind speed retreval
+    cityName.innerText = apiData.city.name;
+    main.innerText = apiData.list[0].weather[0].main;
+    temp.innerText = Math.round(apiData.list[0].main.temp);
+    feel.innerText = Math.round(apiData.list[0].main.feels_like);
+    wind.innerText = Math.round(apiData.list[0].wind.speed)
 }
 apiCall();
 
 
 
-//Loop through retrieved data 
+//For 5 day forecast - loop through retrieved data - change cnt (count) in fetch url
 //  for(var i=0; i<apiData.list.length; i+=8){
 //      console.log(apiData.list[i].dt_txt + " ---> " + apiData.list[i].main.temp  + " ---> " /*+ JSON.stringify(apiData.list[i].weather)*/);
